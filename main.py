b07ee4cd83d6896571c751da0624ad33801e67e4
mezdahun <mezeydavid@gmail.com>
2022-08-17 15:43:29 +0200

    generate fig3

--- main.py
+++ main.py
@@ -195,41 +195,85 @@ import numpy as np
 # plt.show()
 
 
-# Generating figure 3
-cs = []
-rs = []
-fig, ax = plt.subplots(2, 1, figsize=(8, 8), sharex=True)
-a_s = [0.1, 0.2, 1, 3]
-r_inf = 100
-Tau_r = 10
-colors = cm.jet(np.linspace(0, 1, len(a_s)))
+# # Generating figure 3.1
+# cs = []
+# rs = []
+# fig, ax = plt.subplots(2, 1, figsize=(8, 8), sharex=True)
+# a_s = [0.1, 0.25, 0.5, 0.75, 1]
+# r_inf = 500
+# Tau_r = 10
+# colors = cm.jet(np.linspace(0, 1, len(a_s)))
+#
+# t_max = 100
+# dt = 0.01
+# t = np.arange(0, t_max, dt)
+# for i, a in enumerate(a_s):
+#     # defining model with given parameters
+#     mod = CoronaMeasureModel(t_max, dt, Tau_r=10, A=a, r_inf=r_inf, linear=False)  # non-linear model parameters
+#     # evolving system
+#     mod.run()
+#
+#     # collecting data
+#     c, r = mod.get_data()
+#     cs.append(c)
+#     rs.append(r)
+#
+#     # showing data
+#     col = colors[i]
+#     plt.axes(ax[0])
+#     plt.plot(t, c, label=f"$a$={a}", color=col)
+#     if i == len(a_s) - 1:
+#         plt.axhline(y=r_inf, color='black', linestyle='--', label=f"$r_\\infty$")
+#     plt.axes(ax[1])
+#     plt.plot(t, r, label=f"$a$={a}", color=col)
+#     if i == len(a_s) - 1:
+#         plt.axhline(y=r_inf, color='black', linestyle='--', label=f"$r_\\infty$")
+#
+# plt.axes(ax[0])
+# plt.legend(loc="upper right")
+# plt.ylabel("Positive Cases")
+# plt.axes(ax[1])
+# plt.ylabel("Reported Cases ~ Measures")
+# plt.xlabel("t [days]")
+# plt.show()
 
-t_max = 100
-dt = 0.01
-t = np.arange(0, t_max, dt)
-for i, a in enumerate(a_s):
-    # defining model with given parameters
-    mod = CoronaMeasureModel(t_max, dt, Tau_r=10, A=a, r_inf=r_inf, linear=False)  # non-linear model parameters
-    # evolving system
-    mod.run()
-
-    # collecting data
-    c, r = mod.get_data()
-    cs.append(c)
-    rs.append(r)
-
-    # showing data
-    col = colors[i]
-    plt.axes(ax[0])
-    plt.plot(t, c * 1e5, label=f"$a$={a}", color=col)
-    plt.axes(ax[1])
-    plt.plot(t, r * 1e5, label=f"$a$={a}", color=col)
-
-plt.axes(ax[0])
-plt.legend(loc="upper left")
-plt.ylabel("Positive Cases")
-plt.axes(ax[1])
-plt.legend(loc="upper left")
-plt.ylabel("Reported Cases ~ Measures")
-plt.xlabel("t [days]")
-plt.show()
+# # Generating figure 3.2
+# cs = []
+# rs = []
+# fig, ax = plt.subplots(2, 1, figsize=(8, 8), sharex=True)
+# r_infs = [0, 50, 250, 500, 1000]
+# a = 0.1
+# Tau_r = 10
+# colors = cm.jet(np.linspace(0, 1, len(r_infs)))
+#
+# t_max = 100
+# dt = 0.01
+# t = np.arange(0, t_max, dt)
+# for i, r_inf in enumerate(r_infs):
+#     # defining model with given parameters
+#     mod = CoronaMeasureModel(t_max, dt, Tau_r=Tau_r, A=a, r_inf=r_inf, linear=False)  # non-linear model parameters
+#     # evolving system
+#     mod.run()
+#
+#     # collecting data
+#     c, r = mod.get_data()
+#     cs.append(c)
+#     rs.append(r)
+#
+#     # showing data
+#     col = colors[i]
+#     plt.axes(ax[0])
+#     plt.plot(t, c, label=f"$r_{{\\infty}}$={r_inf}", color=col)
+#     plt.axhline(y=r_inf, color=col, linestyle='--')
+#     plt.axes(ax[1])
+#     plt.plot(t, r, label=f"$r_{{\\infty}}$={r_inf}", color=col)
+#     plt.axhline(y=r_inf, color=col, linestyle='--')
+#
+# plt.axes(ax[0])
+# plt.legend(loc="upper right")
+# plt.ylabel("Positive Cases")
+# plt.ylim((-500, 10000))
+# plt.axes(ax[1])
+# plt.ylabel("Reported Cases ~ Measures")
+# plt.xlabel("t [days]")
+# plt.show()
